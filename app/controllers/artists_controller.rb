63acc44f1eb2bda30e728aee0ea1db7d3a3e6050
Winston Pemberton <winstonpemberton@gmail.com>
2020-08-06 22:04:00 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -26,7 +26,7 @@ class ArtistsController < ApplicationController
   private
 
   def artist_params(*args)
-    params.require(:artist).permit(*args)
+    params.require(:artist).permit(:name, :bio)
   end
 
 end
