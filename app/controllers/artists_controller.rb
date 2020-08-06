93f63e674625105125997c801e30432ff998e899
Winston Pemberton <winstonpemberton@gmail.com>
2020-08-06 21:55:22 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -9,11 +9,11 @@ class ArtistsController < ApplicationController
 	end
 
   def create
-  @artist = Artist.new
-  @artist.name = params[:name]
-  @artist.bio = params[:bio]
-  @artist.save
-  redirect_to artist_path(@artist)
-end
+    @artist = Artist.new
+    @artist.name = params[:name]
+    @artist.bio = params[:bio]
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
 
 end
