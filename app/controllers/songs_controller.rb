5674e72576941ff67845eb1675355f45c5353d4c
Winston Pemberton <winstonpemberton@gmail.com>
2020-08-06 22:15:19 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,36 @@
 class SongsController < ApplicationController
+
+  def show
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Genre.new
+  end
+
+  def create
+    @song = Genre.create(song_params)
+    redirect_to song_path(@song)
+  end
+
+  def edit
+    @song = Genre.find(params[:id])
+  end
+
+  def update
+    @song = Genre.find(params[:id])
+    @song.update(song_params)
+    redirect_to song_path(@song)
+  end
+
+  private
+
+  def song_params(*args)
+    params.require(:genre).permit(:name, :artist_id, :genre_id)
+  end
+
 end
