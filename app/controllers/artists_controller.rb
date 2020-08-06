9ba5e9a83fb93b2d4e9febf22f35189ee541b4b0
Winston Pemberton <winstonpemberton@gmail.com>
2020-08-06 22:02:51 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -18,9 +18,9 @@ class ArtistsController < ApplicationController
 	end
 
   def update
-    @school_class = SchoolClass.find(params[:id])
-    @school_class.update(params.require(:school_class).permit(:title, :room_number))
-    redirect_to school_class_path(@school_class)
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params)
+    redirect_to artist_path(@artist)
   end
 
   private
